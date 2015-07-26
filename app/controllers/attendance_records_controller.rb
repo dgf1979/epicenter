387a7874a03890c00e07399766cb8b501fb2aaf8
dgf1979 <drew@finstrom.us>
2015-07-25 20:34:32 -0700

    AJAX-ify attendance check in/out

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -7,25 +7,36 @@ class AttendanceRecordsController < ApplicationController
 
   def create
     @attendance_record = AttendanceRecord.new(attendance_record_params)
-    if @attendance_record.save
-      flash[:notice] = "Welcome #{@attendance_record.student.name}"
-      flash[:secure] =  view_context.link_to("Not you?",
-                        attendance_record_path(@attendance_record),
-                        data: {method: :delete})
-      redirect_to attendance_path
-    else
-      flash[:alert] = "Something went wrong: " + @attendance_record.errors.full_messages.join(", ")
-      redirect_to attendance_path
+    @students = current_admin.current_cohort.students.order(:name)
+    respond_to do |format|
+      if @attendance_record.save
+        flash[:notice] = "Welcome #{@attendance_record.student.name}"
+        flash[:secure] =  view_context.link_to("Not you?",
+                          attendance_record_path(@attendance_record),
+                          data: {method: :delete})
+        format.html { redirect_to attendance_path }
+        format.js { @student = @attendance_record.student }
+      else
+        flash[:alert] = "Something went wrong: " + @attendance_record.errors.full_messages.join(", ")
+        format.html { redirect_to attendance_path }
+        format.js {}
+      end
     end
   end
 
   def update
     @attendance_record = AttendanceRecord.find(params[:id])
-    if @attendance_record.update(attendance_record_params)
-      redirect_to attendance_path, notice: "#{@attendance_record.student.name} successfully updated."
-    else
-      flash[:alert] = "Something went wrong: " + @attendance_record.errors.full_messages.join(", ")
-      redirect_to attendance_path
+    @students = current_admin.current_cohort.students.order(:name)
+    respond_to do |format|
+      if @attendance_record.update(attendance_record_params)
+        flash[:notice] = "#{@attendance_record.student.name} successfully updated."
+        format.html { redirect_to attendance_path }
+        format.js { @student = @attendance_record.student }
+      else
+        flash[:alert] = "Something went wrong: " + @attendance_record.errors.full_messages.join(", ")
+        format.html { redirect_to attendance_path }
+        format.js { @student = @attendance_record.student }
+      end
     end
   end
 
